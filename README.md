82e18e26fc143ba031b990ac01e5d3090368117a
RifatArefin32 <arefinrifat786@gmail.com>
2024-06-10 18:12:14 +0600

    10062024 1812

--- README.md
+++ README.md
@@ -12,14 +12,18 @@
 - Configure the web.php to perform REST
 - Use postman to check the CRUD of users
 
-# Follow the following tutorial for implementing the basics of REST 
+## Follow the following steps for implementing the basics of REST 
 - Create a table "expense" using migration
 - Generate a Model and CRUD files using gii
-- [Generate a Module](https://www.youtube.com/watch?v=1OmNBN5CuUs&list=PLMhOp68dQOeaIIuQ6nh-VqjKxmf9RsE18&index=7)
+- Generate a module and Create a ExpenseController
 - Configure the module in the config/web.php (see 'modules' in the config file)
-- Perform 'http://localhost:8080/module_id' to check it works perfectly
+- Perfrom GET request for expense/ particular expense
+- Configure the request to define the parsers at config/web.php
 - 
-    - 
+- 
 
-
+## Follow Step by Step to Learn Yii2 REST Basics
+- [Generate a Model and CRUD using /gii](https://www.youtube.com/watch?v=OVOy00vdCdY&list=PLMhOp68dQOeaIIuQ6nh-VqjKxmf9RsE18&index=7)
+- [Generate Module to setup REST APIs and Perform GET request](https://www.youtube.com/watch?v=1OmNBN5CuUs&list=PLMhOp68dQOeaIIuQ6nh-VqjKxmf9RsE18&index=6)
+- [Perform CREATE, UPDATE and DELETE request methods]()
 
---
+++ modules/budget/controllers/ExpenseController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace app\modules\budget\controllers;
+use app\models\Expense;
+use Yii;
+use yii\web\NotFoundHttpException;
+/**
+ * Default controller for the `budget` module
+ */
+class ExpenseController extends \yii\rest\ActiveController
+{
+   public $modelClass = 'app\models\Expense';
+
+   public function actions(){
+        $actions = parent::actions();
+        unset($actions['create']);
+        unset($actions['delete']);
+        unset($actions['update']);
+        return $actions;
+   }
+
+   public function actionCreate(){
+        $model = new $this->modelClass;
+        if($model->load(Yii::$app->request->post(), '')){
+            if($model->validate()){
+                $model->remainingAmount();
+                $model->save();
+                return(
+                    [
+                        'status'=> 'Success',
+                        "data"=> $model,
+                    ]
+                );
+            }
+            else{
+                return [
+                    "status"=> "success",
+                    "errors"=> $model->errors,
+                ];
+            }
+        }
+   }
+
+   public function actionDelete($id)
+    {
+        $model = $this->findModel($id);
+        
+        if ($model->delete()) {
+            return [
+                'status' => 'success',
+                'message' => 'Expense field deleted successfully.',
+            ];
+        } else {
+            Yii::$app->response->statusCode = 500;
+            return [
+                'status' => 'error',
+                'message' => 'Failed to delete the expense field.',
+            ];
+        }
+    }
+
+    public function actionUpdate($id)
+    {
+        $model = $this->findModel($id);
+        $model->load(Yii::$app->request->post(), '');
+        $model->remainingAmount();
+        if ($model->validate() && $model->save()) {
+            return [
+                'status' => 'success',
+                'message' => 'Expense updated successfully.',
+                'data' => $model,
+            ];
+        } else {
+            Yii::$app->response->statusCode = 422;
+            return [
+                'status' => 'error',
+                'errors' => $model->errors,
+            ];
+        }
+    }
+
+    protected function findModel($id)
+    {
+        if (($model = Expense::findOne($id)) !== null) {
+            return $model;
+        }
+        throw new NotFoundHttpException('The requested expense field does not exist.');
+    }
+}
