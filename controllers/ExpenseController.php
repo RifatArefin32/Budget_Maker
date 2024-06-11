e2908eceadaa127e4319784d86b723e230b7175a
RifatArefin32 <arefinrifat786@gmail.com>
2024-06-11 14:10:12 +0600

    add new expense action button

--- controllers/ExpenseController.php
+++ controllers/ExpenseController.php
@@ -99,6 +99,8 @@ class ExpenseController extends Controller
         if($this->request->isPost) {
             if($model->load($this->request->post())) {
                 $model->remainingAmount();
+                $addNew = \Yii::$app->request->post('addnew');
+                $model->spent += $addNew;
                 if($model->validate() && $model->save()) {
                     return $this->redirect(['view', 'id' => $model->id]);
                 }
