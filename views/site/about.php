d2d13731614ddbc380722722a8035904a05d8ab8
RifatArefin32 <arefinrifat786@gmail.com>
2024-06-11 16:03:22 +0600

    footer fixed

--- views/site/about.php
+++ views/site/about.php
@@ -6,13 +6,16 @@ use yii\helpers\Html;
 
 $this->title = 'About';
 $this->params['breadcrumbs'][] = $this->title;
+
 ?>
 <div class="site-about">
+    
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        This is the About page. You may modify the following file to customize its content:
+        Budget Maker is a practice application to implement Basic REST API 
     </p>
-
-    <code><?= __FILE__ ?></code>
+    
+    
+    File Directory: <code><?= __FILE__ ?></code>
 </div>
